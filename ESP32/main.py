0320ce627dc5cd6c0936a7163c3a78ca2e70545b
leonbos1 <ktmleon@live.nl>
2022-10-13 15:41:14 +0200

    esp32 crash fix
    probleem opgelost:
    wanneer er geen verbinding is met api, crasht de esp32

--- ESP32/main.py
+++ ESP32/main.py
@@ -1,5 +1,45 @@
+from machine import Pin, ADC, deepsleep
+import time
+import network
+import urequests
+import json
+
 def main():
-    print('mounted succes')
+    connect()
+    
+    adc = ADC(Pin(32))
+    adc.width(ADC.WIDTH_10BIT)
+    adc.atten(ADC.ATTN_6DB)
+
+        
+    temperature = adc.read()
+    temperature = temperature / 1023
+    temperature = temperature * 2
+    temperature = temperature - 0.5
+    temperature = temperature * 100
+    print("Temperature", temperature, " , afgerond", int(round(temperature)))
+    send_temp(round(temperature,2))
+
+    
+def send_temp(temperature):
+    url = "http://ronleon.nl:5000/"
+ 
+    data = {
+        'degrees':temperature
+        }
+    json_object = json.dumps(data)
+    
+    request = urequests.post(url, data=json_object)
+    
+    request.close()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+        deepsleep(60000)
+    except:
+        deepsleep(1000)
+
+
+
+
