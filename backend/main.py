ee5e93f44a47298ec99cc1079a93cf799684788c
leonbos1 <ktmleon@live.nl>
2022-10-14 19:55:17 +0200

    API V2 start

--- backend/main.py
+++ backend/main.py
@@ -4,52 +4,78 @@ import sqlite3
 import json
 import datetime
 from flask_cors import CORS, cross_origin
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 CORS(app)
 
+class Temperature:
+    def __init__(self):
+        self.conn = sqlite3.connect('temperature.db')
+        self.cur = self.conn.cursor()
+        f = open("pw.txt","r")
+        self.password = f.readlines()[0]
+        
+    def get(self):
+        return "", 404
 
-@app.route('/', methods=['GET'])
-def index():
-    return "", 404
+    def delete(self, id):
+        try:
+            if request.headers['token'] == 'ABHJ':
+                conn = sqlite3.connect('data.db')
+                cur = conn.cursor()
+                cur.execute(f"DELETE FROM temperatures WHERE id = {id}")
+                conn.commit()
+                conn.close()
+                return "succes", 200
+        except:
+            return "unauthorized", 401
 
-@app.route('/<id>', methods=['DELETE'])
-def delete(id):
-    try:
+    def post(self):
+        input_json = request.get_json(force=True)
+        conn = sqlite3.connect('data.db')
+        cur = conn.cursor()
+
+        temp = round(input_json['degrees'], 2)
+
+        date = datetime.date.today()
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+
+        avgtemp = get_last_temp()
+
+        max_deviation = 1.5
+
+        if temp > avgtemp + max_deviation or temp < avgtemp - max_deviation:
+            
+            temp = avgtemp
+            
+        cur.execute(f"INSERT INTO temperatures (degrees, date, time) VALUES ({temp}, '{date}', '{time}')")
+        conn.commit()
+        conn.close()
+
+        return "succes", 200
+
+    def put(self, id):
         if request.headers['token'] == 'ABHJ':
+            input_json = request.get_json(force=True)
             conn = sqlite3.connect('data.db')
             cur = conn.cursor()
-            cur.execute(f"DELETE FROM temperatures WHERE id = {id}")
+            cur.execute(f"UPDATE temperatures SET degrees = {input_json['degrees']} WHERE id = {id}")
             conn.commit()
             conn.close()
             return "succes", 200
-    except:
+
         return "unauthorized", 401
 
-@app.route('/',methods=['POST'])
-def post():
-    input_json = request.get_json(force=True)
-    conn = sqlite3.connect('data.db')
-    cur = conn.cursor()
-
-    temp = round(input_json['degrees'], 2)
-
-    date = datetime.date.today()
-    time = datetime.datetime.now().strftime("%H:%M:%S")
-
-    avgtemp = get_last_temp()
-
-    max_deviation = 1.5
-
-    if temp > avgtemp + max_deviation or temp < avgtemp - max_deviation:
+    #@app.route('/login', methods=["POST"])
+    def login(self):
+        input_json = request.get_json(force=True)
         
-        temp = avgtemp
-        
-    cur.execute(f"INSERT INTO temperatures (degrees, date, time) VALUES ({temp}, '{date}', '{time}')")
-    conn.commit()
-    conn.close()
 
-    return "succes", 200
+        if input_json['password'] == self.password:
+            return "ABHJ", 200
+        return "unauthorized", 401
 
 
 def get_last_temp():
@@ -65,19 +91,6 @@ def get_last_temp():
     avg_temp = round(avg_temp / len(temps), 2)
     conn.close()
     return avg_temp
-
-@app.route('/<id>', methods=['PUT'])
-def put(id):
-    if request.headers['token'] == 'ABHJ':
-        input_json = request.get_json(force=True)
-        conn = sqlite3.connect('data.db')
-        cur = conn.cursor()
-        cur.execute(f"UPDATE temperatures SET degrees = {input_json['degrees']} WHERE id = {id}")
-        conn.commit()
-        conn.close()
-        return "succes", 200
-
-    return "unauthorized", 401
 
 @app.route('/current_temp', methods=['GET'])
 def current_temp():
@@ -134,15 +147,9 @@ def weekly():
     except:
         return "unauthorized", 401
 
-@app.route('/login', methods=["POST"])
-def login():
-    input_json = request.get_json(force=True)
-    f = open("pw.txt","r")
-    password = f.readlines()[0]
 
-    if input_json['password'] == password:
-        return "ABHJ", 200
-    return "unauthorized", 401
+api.add_resource(Temperature, "/<int:id>")
 
-app.run(host='192.168.178.220', debug=True)
+if __name__ == "__main__":
+    app.run(host='192.168.178.220', debug=True)
 #test
