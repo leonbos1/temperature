2d3024a94a72ee6c116266479a8acae49e5c60ae
leonbos1 <ktmleon@live.nl>
2022-10-15 09:30:12 +0200

    api 2.0 update

--- backend/main.py
+++ backend/main.py
@@ -10,19 +10,23 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-class Temperature:
+class Temperature(Resource):
     def __init__(self):
-        self.conn = sqlite3.connect('temperature.db')
+        self.conn = sqlite3.connect('data.db')
         self.cur = self.conn.cursor()
         f = open("pw.txt","r")
+        t = open("token.txt","r")
         self.password = f.readlines()[0]
+        self.token = t.readlines()[0]
+        f.close()
+        t.close()
         
     def get(self):
         return "", 404
 
     def delete(self, id):
         try:
-            if request.headers['token'] == 'ABHJ':
+            if request.headers['token'] == self.token:
                 conn = sqlite3.connect('data.db')
                 cur = conn.cursor()
                 cur.execute(f"DELETE FROM temperatures WHERE id = {id}")
@@ -57,7 +61,7 @@ class Temperature:
         return "succes", 200
 
     def put(self, id):
-        if request.headers['token'] == 'ABHJ':
+        if request.headers['token'] == self.token:
             input_json = request.get_json(force=True)
             conn = sqlite3.connect('data.db')
             cur = conn.cursor()
@@ -74,8 +78,74 @@ class Temperature:
         
 
         if input_json['password'] == self.password:
-            return "ABHJ", 200
+            return self.token, 200
         return "unauthorized", 401
+
+
+class Weekly(Resource):
+    def __init__(self):
+        self.conn = sqlite3.connect('data.db')
+        self.cur = self.conn.cursor()
+        f = open("pw.txt","r")
+        t = open("token.txt","r")
+        self.password = f.readlines()[0]
+        self.token = t.readlines()[0]
+        f.close()
+        t.close()
+
+    def get(self):
+        try:
+            if request.headers['token'] == self.token:
+
+                self.cur.execute("select * from temperatures order by id asc limit 10500")
+                result = json.dumps(self.cur.fetchall())
+
+                result_json = json.loads(result)
+                today = datetime.datetime.now()
+                last_week = today - datetime.timedelta(days=7)
+
+                to_return = []
+
+                for e in result_json:
+                    datetime_string = e[2]
+                    datetimeobj=datetime.datetime.strptime(datetime_string, "%Y-%m-%d")
+                    
+                    if datetimeobj >= last_week:
+                        to_return.append(e)
+                
+                return json.dumps(str(to_return))
+        except:
+            return "unauthorized", 401
+
+class CurrentTemp(Resource):
+
+    def __init__(self):
+        self.conn = sqlite3.connect('data.db')
+        self.cur = self.conn.cursor()
+        f = open("pw.txt","r")
+        t = open("token.txt","r")
+        self.password = f.readlines()[0]
+        self.token = t.readlines()[0]
+        f.close()
+        t.close()
+
+    def current_temp(self):
+        try:
+            if request.headers['token'] == self.token:
+
+                self.cur.execute("select * from temperatures order by id desc limit 5")
+
+                result = json.dumps(self.cur.fetchall())
+                result_json = json.loads(result)
+
+                avg_temp = 0
+
+                for e in result_json:
+                    avg_temp += e[1]
+
+                return str(round(avg_temp/5,2))
+        except:
+            return "unauthorized", 401
 
 
 def get_last_temp():
@@ -92,63 +162,10 @@ def get_last_temp():
     conn.close()
     return avg_temp
 
-@app.route('/current_temp', methods=['GET'])
-def current_temp():
-    try:
-        if request.headers['token'] == 'ABHJ':
-            con = sqlite3.connect('data.db')
-            cur = con.cursor()
-
-            cur.execute("select * from temperatures order by id desc limit 5")
-
-            result = json.dumps(cur.fetchall())
-            result_json = json.loads(result)
-
-            avg_temp = 0
-
-            for e in result_json:
-                avg_temp += e[1]
-
-            con.close()
-
-            return str(round(avg_temp/5,2))
-    except:
-        return "unauthorized", 401
-
-@app.route('/weekly', methods=["GET"])
-def weekly():
-    try:
-        if request.headers['token'] == 'ABHJ':
-            con = sqlite3.connect('data.db')
-            cur = con.cursor()
-
-            cur.execute("select * from temperatures order by id asc limit 10500")
-
-            result = json.dumps(cur.fetchall())
-
-            con.close()
-
-            result_json = json.loads(result)
-
-            today = datetime.datetime.now()
-
-            last_week = today - datetime.timedelta(days=7)
-
-            to_return = []
-
-            for e in result_json:
-                datetime_string = e[2]
-                datetimeobj=datetime.datetime.strptime(datetime_string, "%Y-%m-%d")
-                
-                if datetimeobj >= last_week:
-                    to_return.append(e)
-            
-            return json.dumps(str(to_return))
-    except:
-        return "unauthorized", 401
-
-
 api.add_resource(Temperature, "/<int:id>")
+api.add_resource(Weekly, "/weekly")
+api.add_resource(CurrentTemp, "/current_temp")
+
 
 if __name__ == "__main__":
     app.run(host='192.168.178.220', debug=True)
