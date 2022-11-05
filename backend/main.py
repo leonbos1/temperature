db1b494458d77e9e0e73986bf2f684b06e39cc7e
leonbos1 <ktmleon@live.nl>
2022-11-05 20:58:27 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -379,4 +379,4 @@ api.add_resource(Extra, "/extra")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='192.168.178.69',port=1000, debug=True, threaded=True)
+    app.run(host='192.168.178.220',port=5000, debug=True, threaded=True)
