54c5fa3def6fdd310f329c19894828dcb5f476e4
leonbos1 <ktmleon@live.nl>
2022-11-12 17:30:58 +0100

    Update cache.py

--- backend/cache.py
+++ backend/cache.py
@@ -10,8 +10,18 @@ cur = conn.cursor()
 cur.execute("SELECT degrees, date, time FROM temperatures LIMIT 40500")
 
 data = cur.fetchall()
-print(data[0][1])
 
-#for i in data:
- #   current_date = i[1]
-        
+current_date = data[0][1]
+temps = []
+
+temperature_per_day = {}
+
+for i in data:
+    if current_date == i[1]:
+        temps.append(i[0])
+    else:
+        temperature_per_day[current_date] = sum(temps) / len(temps)
+        current_date = i[1]
+        temps = []
+
+print(temperature_per_day)
