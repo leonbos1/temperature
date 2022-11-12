6326363aa3a4266242620b9f98859c14199a831d
leonbos1 <ktmleon@live.nl>
2022-11-12 17:38:15 +0100

    Update cache.py

--- backend/cache.py
+++ backend/cache.py
@@ -23,6 +23,10 @@ for i in data:
         temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
         current_date = i[1]
         temps = []
+ 
+    if i == data[-1]:
+        temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
+
 
 cur.execute("DELETE FROM average_temperatures")
 conn.commit()
