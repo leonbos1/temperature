a7c424d0752c57064e8109ca7dd15594749fbb28
leonbos1 <ktmleon@live.nl>
2022-11-12 17:23:42 +0100

    Create cache.py

---
+++ backend/cache.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+# alle data opslaan tot en met een maand geleden
+# van alle dagen het gemiddelde berekenen
+# in een dictionary opslaan
+# in database opslaan
+
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
+cur.execute("SELECT degrees FROM temperatures ORDER BY id DESC LIMIT 40500")
+
+data = cur.fetchall()
+
+for i in data:
+    print(i)
