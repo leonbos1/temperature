449476a2c0c4c9f7ac7e3e681f102ff291bcc621
leonbos1 <ktmleon@live.nl>
2022-11-13 17:59:45 +0100

    Update cache.py

--- backend/cache.py
+++ backend/cache.py
@@ -1,38 +1,41 @@
 import sqlite3
-
-# alle data opslaan tot en met een maand geleden
-# van alle dagen het gemiddelde berekenen
-# in een dictionary opslaan
-# in database opslaan
+from time import sleep
 
 conn = sqlite3.connect('data.db')
 cur = conn.cursor()
-cur.execute("SELECT degrees, date, time FROM temperatures LIMIT 40500")
 
-data = cur.fetchall()
+def main():
+    cur.execute("SELECT degrees, date, time FROM temperatures LIMIT 40500")
 
-current_date = data[0][1]
-temps = []
+    data = cur.fetchall()
 
-temperature_per_day = {}
+    current_date = data[0][1]
+    temps = []
 
-for i in data:
-    if current_date == i[1]:
-        temps.append(i[0])
-    else:
-        temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
-        current_date = i[1]
-        temps = []
- 
-    if i == data[-1]:
-        temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
+    temperature_per_day = {}
+
+    for i in data:
+        if current_date == i[1]:
+            temps.append(i[0])
+        else:
+            temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
+            current_date = i[1]
+            temps = []
+    
+        if i == data[-1]:
+            temperature_per_day[current_date] = round(sum(temps) / len(temps),2)
 
 
-cur.execute("DELETE FROM average_temperatures")
-conn.commit()
+    cur.execute("DELETE FROM average_temperatures")
+    conn.commit()
 
-for i in temperature_per_day:
-    print(i, temperature_per_day[i])
-    cur.execute("INSERT INTO average_temperatures VALUES (?,?)", (i, temperature_per_day[i]))
+    for i in temperature_per_day:
+        print(i, temperature_per_day[i])
+        cur.execute("INSERT INTO average_temperatures VALUES (?,?)", (i, temperature_per_day[i]))
 
-conn.commit()
+    conn.commit()
+
+if __name__ == "__main__":
+    while True:
+        sleep(60)
+        main()
