3587610c2dcefe45c7373e22d2fe52a7adb7dc2c
leonbos1 <ktmleon@live.nl>
2023-04-22 10:12:36 +0200

    g

--- backend/create_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('data.db')
-cur = conn.cursor()
-
-#create new table with the average temperature per day
-cur.execute("CREATE TABLE IF NOT EXISTS average_temperatures (id TEXT, degrees REAL)")
-conn.commit()
