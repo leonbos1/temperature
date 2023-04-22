d9141d62d869775e6cbd136fb47ef6003fb43a39
leonbos1 <ktmleon@live.nl>
2023-04-22 10:02:58 +0200

    Revert "revert changes"
    This reverts commit 31e4d64f226e8edc51601f733ff7c79450a6c72b.

--- frontend/temperature/src/views/home.vue
+++ frontend/temperature/src/views/home.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div class="mt-12">
     <div class="p-4 xl:ml-80">
       <div class="">
@@ -20,13 +21,23 @@
 
     <card-row :sensor_id="sensor_id" />
     <graph-row :sensor_id="sensor_id" />
+=======
+  <div class="content">
+      <Visitors class="visitors" />
+      <CurrentTemp />
+>>>>>>> parent of aa8562a (frontend updates)
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import CardRow from "../components/card-row.vue";
 import GraphRow from "../components/graph-row.vue";
 import datajson from "../data.json";
+=======
+import CurrentTemp from "./current-temp.vue";
+import Visitors from "./visitors.vue";
+>>>>>>> parent of aa8562a (frontend updates)
 
 export default {
   name: "HomePage",
@@ -55,13 +66,14 @@ export default {
   },
 
   components: {
-    CardRow,
-    GraphRow,
+    CurrentTemp,
+    Visitors,
   },
 };
 </script>
 
 <style scoped>
+
 .content {
   margin-top: 10vh;
   display: flex;
@@ -72,5 +84,6 @@ export default {
   width: 70%;
   background-color: #f5f5f5;
 }
+
 </style>
  
